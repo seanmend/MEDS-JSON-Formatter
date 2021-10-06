e0633ae0d67e5f2ade794d5829227821e0c79dbc
Sean Doyle <sean@mendfamily.com>
2021-10-06 13:16:21 -0400

    Works.

---
+++ main.py
@@ -0,0 +1,181 @@
+import sys
+import json
+
+
+# pass csv when calling. ex:python3 main.py file.csv
+def meds_json_formatter(json_path):
+    output_list = []
+    if not json_path:
+        raise RuntimeError
+    print(json_path)
+    # Opening JSON file
+    f = open(json_path, )
+
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+
+    # count = 0
+    #
+    values = data.values()
+    # for v in values:
+    #     count += 1
+    keys = data.keys()
+    print('keys: ', keys)
+    print(keys)
+    print('-------------------')
+    print('values: ', values)
+    length_of_list = len(keys)
+    print(length_of_list)
+
+    # Format existing
+
+    for i in range(1, length_of_list + 1):
+        print('loop')
+        first_key = list(data)[i - 1]
+        first_val = list(data.values())[i - 1]
+        output_list.append({"code": first_key, "item": first_val})
+
+    # Add New Entry
+    new_key = '1734'  # EDIT
+    new_val = 'Non Medicaid Florida Community Care'  # EDIT
+    output_list.append({"code": new_key, "item": new_val})
+
+    print('OUTPUT:', output_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # print(data)
+
+    # with open(json_path, 'r+') as file:
+    #     # First we load existing data into a dict.
+    #     file_data = json.load(file)
+    #     # Join new_data with file_data inside emp_details
+    #     file_data[x].append(new_data)
+    #     # Sets file's current position at offset.
+    #     file.seek(0)
+    #     # convert back to json.
+    #     json.dump(file_data, file, indent=4)
+
+
+# python object to be appended
+# y = {"emp_name": "Nikhil",
+#      "email": "nikhil@geeksforgeeks.org",
+#      "job_profile": "Full Time"
+#      }
+#
+# write_json(y)
+
+
+
+
+
+
+    # count = 0
+    #
+    values = data.values()
+    # for v in values:
+    #     count += 1
+    keys = data.keys()
+    # print('keys: ', keys)
+    # print(keys.type())
+    # print('-------------------')
+    # print('values: ', values)
+
+    # length_of_list = len(keys)
+    # print(length_of_list)
+
+    # for n in range(count):
+    #     print('n:::', n)
+    #     output_list.append({"code": keys[n], "item": values[n]})
+    #     json.dump(data, file, indent=4)
+    #
+    # print('output::::', output_list)
+
+    #
+    # for l in length_of_list:
+    #     output.append(l)
+
+    # for x in data:
+    #     print(data[x])
+
+    # output = [{"code": keys, "item": values} ]
+    # print(output)
+
+    # output_list = dict(zip(keys, values))
+
+    # for key in keys:
+    #     print('v:::::', value)
+    #     output_list.append({key, value})
+    #
+    # for value in values:
+    #     print('v:::::', value)
+    #     output_list.append({key, value})
+
+    # print('OUTPUT:::::::::::::::::::::::::')
+    # print(output_list)
+    # # Closing file
+    # f.close()
+
+
+
+
+
+
+
+
+
+
+
+    # Iterating through the json
+    # list
+    # values = data.values()
+    # keys = data.keys()
+    # print('keys: ', keys)
+    # print('-------------------')
+    # print('values: ', values)
+    #
+    # output_list = []
+    #
+    # for key in keys:
+    #     print('v:::::', value)
+    #     output_list.append({key, value})
+    #
+    # for value in values:
+    #     print('v:::::', value)
+    #     output_list.append({key, value})
+    #
+    # print('OUTPUT:::::::::::::::::::::::::')
+    # print(output_list)
+    # # Closing file
+    # f.close()
+
+    # save output as text file
+    # text_file = open('output.txt', 'w')
+    # for element in text_file:
+    #     text_file.write(element + "\n")
+    # text_file.close()
+
+
+if __name__ == '__main__':
+    json_path = sys.argv[1]
+    meds_json_formatter(json_path)
+
---
+++ readme.md
@@ -0,0 +1,26 @@
+meds_json_formatter
+
+GOAL: FORMAT JSON FILE 
+
+TAKE IN JSON OBJECT
+
+{
+    "1002": "Magellan Behavioral Health of FL",
+    ...
+}
+
+RETURN OBJECT
+
+[
+    {
+        "code": "1002",
+        "item": "Magellan Behavioral Health of FL"
+    },
+    {
+    	 'code': '1002', 
+    	 'item': 'Magellan Behavioral Health of FL'
+    },
+
+
+    ...
+ ]# MEDS-JSON-Formatter
